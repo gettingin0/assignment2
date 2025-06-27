278fe5452a1afe601f3fa5611bebed134425c843
gettingin0 <dipika01202006@gmail.com>
2025-06-27 12:29:08 +0530

    Add files via upload

---
+++ assingment2.py
@@ -0,0 +1,15 @@
+#task 1
+number = int(input('Enter No. : '))
+a = number
+
+if number % 2 == 0 :
+    print('Entered number is Even.')
+else:
+    print('Entered number is Odd.')
+
+#task 2
+n = 0
+for i in range(1,51):
+    n += i
+print('Sum of all integers : ',n)
+
